55ac6ec78f8113eb26e4923f64ac52007e1c5a7b
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2016-05-02 20:55:54 -0400

    magicien update

--- Axzor/Axzor/magicien.cpp
+++ Axzor/Axzor/magicien.cpp
@@ -3,11 +3,11 @@
 
 magicien::magicien()
 {
-	_vie = 0.0;
+	_vie = 10.0;
 	_velocite = 0.0;
-	this->set_x(0);
-	this->set_y(0);
-	this->set_renderer(NULL);
+	_position.x = 0;
+	_position.y = 0;
+	set_renderer(NULL);
 }
 
 
@@ -15,17 +15,19 @@ magicien::~magicien()
 {
 	_vie = 0.0;
 	_velocite = 0.0;
-	this->set_x(0);
-	this->set_y(0);
-	this->set_renderer(NULL);
+	_position.x = 0;
+	_position.y = 0;
+	set_renderer(NULL);
 }
 
 
 magicien::magicien(int x, int y, SDL_Renderer* render)
 {
-	this->set_x(x);
-	this->set_y(y);
-	this->set_renderer(render);
+	_vie = 10.0;
+	_velocite = 0.0;
+	_position.x = x;
+	_position.y = y;
+	set_renderer(render);
 }
 
 
@@ -43,23 +45,68 @@ void magicien::augmenter_vie(int pv)
 
 bool magicien::collision(ennemi badguy)
 {
-	return false;
+	if (detecter_collision(this->get_rect(), badguy.get_rect()) != '0')
+	{
+		this->reduire_vie(POINTS_DEGATS_ME);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
 bool magicien::collision(bloc lebloc)
 {
+	switch (detecter_collision(this->get_rect(), lebloc.get_rect()))
+	{
+	case '0':
+		this->_velocite = 1; //Pas de bloc; tomber
+		break; 
+	case 'h':
+		this->_velocite = 0; //Bloc en haut; stopper le saut
+		break; 
+	case 'b':
+		this->_velocite = 0; //Bloc en bas; stopper la chute
+		break; 
+	case 'g':
+		return true; // Bloc sur le côté gauche
+		break;
+	case 'd':
+		return true; // Bloc sur le côté droit
+		break;
+	default:
+		break;
+	}
+
 	return false;
 }
 
 
 bool magicien::collision(potion lapotion)
 {
-	return false;
+	if (detecter_collision(this->get_rect(), lapotion.get_rect()))
+	{
+		this->augmenter_vie(POINTS_VIE_POTION);
+		lapotion.~potion();
+	}
+	else
+	{
+		return false;
+	}
+
 }
 
 
 bool magicien::collision(staff baton)
 {
-	return false;
+	if (detecter_collision(this->get_rect(), baton.get_rect()))
+	{
+		this->_type = baton.get_type();
+	}
+	else
+	{
+		return false;
+	}
 }
