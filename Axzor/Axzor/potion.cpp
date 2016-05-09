33c69c7e4d5fd2942819ebb9e72203bec59209c2
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2016-05-09 08:32:53 -0400

    potion...nonupdate?
    La potion semble avoir été ignorée. Re-commit.

--- Axzor/Axzor/potion.cpp
+++ Axzor/Axzor/potion.cpp
@@ -3,17 +3,23 @@
 
 potion::potion()
 {
+	_position.x = 0;
+	_position.y = 0;
+	set_renderer(NULL);
 }
 
 
 potion::~potion()
 {
+	_position.x = 0;
+	_position.y = 0;
+	set_renderer(NULL);
 }
 
 
 potion::potion(int x, int y, SDL_Renderer* render)
 {
-	this->set_x(x);
-	this->set_y(y);
-	this->set_renderer(render);
+	_position.x = x;
+	_position.y = y;
+	set_renderer(render);
 }
